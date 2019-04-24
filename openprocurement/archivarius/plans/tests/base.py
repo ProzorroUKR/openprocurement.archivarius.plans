24b4b7423cc68b41a6c0d8a1aa6ef1246955ae5d
smithumble <smithumble@gmail.com>
2019-04-24 18:04:55 +0300

    Update tests

--- openprocurement/archivarius/plans/tests/base.py
+++ openprocurement/archivarius/plans/tests/base.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
-import webtest
-from openprocurement.planning.api.tests.base import BasePlanWebTest, PrefixedRequestClass
+from openprocurement.planning.api.tests.base import BasePlanWebTest
 
 
 class BasePlanArchivariusWebTest(BasePlanWebTest):
-
-    def setUp(self):
-        self.app = webtest.TestApp(
-            "config:tests.ini", relative_to=os.path.dirname(__file__))
-        self.app.RequestClass = PrefixedRequestClass
-        self.app.authorization = ('Basic', ('token', ''))
-        self.couchdb_server = self.app.app.registry.couchdb_server
-        self.db = self.app.app.registry.db
-        if self.docservice:
-            self.setUpDS()
-        self.create_plan()
+    relative_to = os.path.dirname(__file__)
